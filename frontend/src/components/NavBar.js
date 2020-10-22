f384928ea182886acefbc2e1a8a09cf7eacd1c95
barreirogustavo05 <barreirogustavo05@gmail.com>
2020-10-22 15:21:38 -0300

    implementando la barra de navegacion

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,43 +1,55 @@
 import React from "react";
 import {Link} from 'react-router-dom'
+import "../styles/Navbar.css"
+
+
 
 const NavBar = () => {
-  return (
-    <nav>
-        <div className="nav-wrapper">
-            <a href="#!" class="brand-logo">
-                 Logo
-            </a>
-            <ul className="right hide-on-med-and-down">
-                <li>
-                    <Link to="#">
-                        <i className="material-icons left">search</i>Search
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/">Inicio</Link>
-                </li>
-                <li>
-                    <Link to="/suppliers">Empresas</Link>
-                </li>
-                <li>
-                    <Link to="/live">En Vivo</Link>
-                </li>
-                <li>
-                    <Link to="/faqs">Preguntas Frecuentes</Link>
-                </li>
-                <li>
-                    <Link to="/howtobuy">Como comprar?</Link>
-                </li>
-                <li>
-                    <Link to="/aboutus">Quienes Somos?</Link>
-                </li>
-                <li>
-                    <Link to="/contact">Contacto</Link>
-                </li>
-            </ul>
-      </div>
-    </nav>
+    return (
+    
+       <div className="NavBar"> 
+           <div className="row">
+               <div className="col s4">
+                   <img className='img-logo' src={require('../images/logo.png')} alt="logo"/>
+               </div>
+               <div className="col s4">
+                   <form className="form-inline">
+                       <input className="form-control sm-2" id='inputSearch' type="search" placeholder="Search" aria-label="Search"/>
+                   </form>
+               </div>
+               <div className="col s4">
+                   <Link to="/">
+                       <i className="material-icons left" id="iconSearch">search</i>
+                   </Link>     
+               </div>
+           </div>           
+
+           <nav className="center-align">
+                <div className="row">
+                    <div className="col">
+                        <Link to="/">Inicio</Link>
+                    </div>
+                    <div className="col">
+                        <Link to="/suppliers">Empresas</Link>
+                    </div>
+                    <div className="col">
+                        <Link to="/live">En Vivo</Link>
+                    </div>
+                    <div className="col">
+                        <Link to="/faqs">Preguntas Frecuentes</Link>
+                    </div>
+                    <div className="col">
+                        <Link to="/howtobuy">Como comprar?</Link>
+                    </div>
+                    <div className="col">
+                        <Link to="/aboutus">Quienes Somos?</Link>
+                    </div>
+                    <div className="col">
+                        <Link to="/contact">Contacto</Link>
+                    </div>
+                </div>
+            </nav>
+    </div>
   );
 };
 
