536719ead2c337a2a3f4b633e1a079d6a87415ae
barreirogustavo05 <barreirogustavo05@gmail.com>
2020-10-22 18:07:36 -0300

    haciendo responsive el navbar y comenzando el footer

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,31 +1,17 @@
 import React from "react";
 import {Link} from 'react-router-dom'
 import "../styles/Navbar.css"
+import M from 'materialize-css'
 
 
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {});
+});
 
-const NavBar = () => {
+const elems = () => {
     return (
-    
-       <div className="NavBar"> 
-           <div className="row">
-               <div className="col s4">
-                   <img className='img-logo' src={require('../images/logo.png')} alt="logo"/>
-               </div>
-               <div className="col s4">
-                   <form className="form-inline">
-                       <input className="form-control sm-2" id='inputSearch' type="search" placeholder="Search" aria-label="Search"/>
-                   </form>
-               </div>
-               <div className="col s4">
-                   <Link to="/">
-                       <i className="material-icons left" id="iconSearch">search</i>
-                   </Link>     
-               </div>
-           </div>           
-
-           <nav className="center-align">
-                <div className="row">
+        <div className="row">
                     <div className="col">
                         <Link to="/">Inicio</Link>
                     </div>
@@ -48,7 +34,55 @@ const NavBar = () => {
                         <Link to="/contact">Contacto</Link>
                     </div>
                 </div>
+    )
+}
+
+const NavBar = () => {
+    
+
+      return (
+    
+       <div className="NavBar"> 
+           <div className="row">
+               <div className="col s4">
+                   <img className='img-logo' src={require('../images/logo.png')} alt="logo"/>
+               </div>
+               <div className="col s4">
+                   <form className="form-inline">
+                       <input className="form-control sm-2" id='inputSearch' type="search" placeholder="Search" aria-label="Search"/>
+                   </form>
+               </div>
+               <div className="col s4">
+                   <Link to="/">
+                       <i className="material-icons left" id="iconSearch">search</i>
+                   </Link>     
+               </div>
+           </div>           
+
+           <nav>
+                <div class="nav-wrapper">
+                <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+                <ul class="right hide-on-med-and-down">
+                    <li><Link to="/">Inicio</Link></li>
+                    <li><Link to="/suppliers">Empresas</Link></li>
+                    <li><Link to="/live">En Vivo</Link></li>
+                    <li><Link to="/faqs">Preguntas Frecuentes</Link></li>
+                    <li><Link to="/howtobuy">Como comprar?</Link></li>
+                    <li><Link to="/aboutus">Quienes Somos?</Link></li>
+                    <li><Link to="/contact">Contacto</Link></li>
+                </ul>
+                </div>
             </nav>
+
+            <ul class="sidenav" id="mobile-demo">
+                <li><Link to="/">Inicio</Link></li>
+                <li><Link to="/suppliers">Empresas</Link></li>
+                <li><Link to="/live">En Vivo</Link></li>
+                <li><Link to="/faqs">Preguntas Frecuentes</Link></li>
+                <li><Link to="/howtobuy">Como comprar?</Link></li>
+                <li><Link to="/aboutus">Quienes Somos?</Link></li>
+                <li><Link to="/contact">Contacto</Link></li>
+            </ul>
     </div>
   );
 };
