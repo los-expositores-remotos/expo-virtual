148d6b48d2a369774ce4f9f5b8bd9846467bea14
adriancardozo <adrian.cardozo1@alu.unq.edu.ar>
2020-10-24 08:56:36 -0300

    Corrijo errores

--- src/test/kotlin/ar/edu/unq/cucumber/visualizacion_de_proveedores/VisualizacionDeProveedoresStepdefs.java
+++ src/test/kotlin/ar/edu/unq/cucumber/visualizacion_de_proveedores/VisualizacionDeProveedoresStepdefs.java
@@ -1,9 +1,5 @@
 package ar.edu.unq.cucumber.visualizacion_de_proveedores;
 
-import cucumber.api.java.en.Given;
-import services.ProveedorService;
-import services.impl.ProveedorServiceImp;
-
 public class VisualizacionDeProveedoresStepdefs {
     //private ProveedorService proveedorService = new ProveedorServiceImp(new MongoProveedorDAO())
 
