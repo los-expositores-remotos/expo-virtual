c4a6860d78a3f56ceeeae4f64696801820b5a274
adriancardozo <adrian.cardozo1@alu.unq.edu.ar>
2020-10-24 08:40:22 -0300

    Agrego primer definicion de pasos

--- src/test/kotlin/ar/edu/unq/cucumber/visualizacion_de_proveedores/VisualizacionDeProveedoresStepdefs.java
+++ src/test/kotlin/ar/edu/unq/cucumber/visualizacion_de_proveedores/VisualizacionDeProveedoresStepdefs.java
@@ -1,8 +1,14 @@
 package ar.edu.unq.cucumber.visualizacion_de_proveedores;
 
 import cucumber.api.java.en.Given;
-import cucumber.api.java.en.When;
+import services.ProveedorService;
+import services.impl.ProveedorServiceImp;
 
 public class VisualizacionDeProveedoresStepdefs {
+    //private ProveedorService proveedorService = new ProveedorServiceImp(new MongoProveedorDAO())
 
+    //@Given("^Un proveedor vacio$")
+    //public void unProveedorVacio() {
+
+    //}
 }
