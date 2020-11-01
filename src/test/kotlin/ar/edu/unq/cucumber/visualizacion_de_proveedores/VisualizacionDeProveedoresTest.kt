ab46251712b734d8794490239388316cddfee0cb
adriancardozo <adrian.cardozo1@alu.unq.edu.ar>
2020-11-01 19:17:20 -0300

    empiezo tests de dao

--- src/test/kotlin/ar/edu/unq/cucumber/visualizacion_de_proveedores/VisualizacionDeProveedoresTest.kt
+++ src/test/kotlin/ar/edu/unq/cucumber/visualizacion_de_proveedores/VisualizacionDeProveedoresTest.kt
@@ -6,5 +6,4 @@ import org.junit.runner.RunWith
 
 @RunWith(Cucumber::class)
 @CucumberOptions(features = ["src/test/resources/ar/edu/unq/cucumber/visualizacion_de_proveedores/"])
-class VisualizacionDeProveedoresTest {
-}
+class VisualizacionDeProveedoresTest
---
+++ src/test/kotlin/ar/edu/unq/mongodb/GenericMongoDAOTest.kt
@@ -0,0 +1,61 @@
+package ar.edu.unq.mongodb
+
+import ar.edu.unq.dao.mongodb.GenericMongoDAO
+import ar.edu.unq.services.runner.DataBaseType
+import ar.edu.unq.services.runner.TransactionRunner
+import ar.edu.unq.services.runner.TransactionRunner.runTrx
+import ar.edu.unq.services.runner.TransactionType
+import org.bson.types.ObjectId
+import org.junit.After
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+abstract class GenericMongoDAOTest<T> {
+
+    lateinit var dao: GenericMongoDAO<T>
+    var items: MutableList<T> = emptyList<T>().toMutableList()
+
+    @Before
+    fun setUp() {
+        this.generarDAO()
+        this.generarItems()
+    }
+
+    abstract fun generarItems()
+
+    abstract fun generarDAO()
+
+    open fun obtenerTodos(): List<T>{
+        return this.dao.getAll()
+    }
+
+/*    @Test
+    fun returnAClearCollection() {
+        val result: List<T> = runTrx({ this.dao.getAll() }, listOf(TransactionType.MONGO), DataBaseType.TEST)
+        Assert.assertEquals(0, result.count())
+    }
+
+    @Test
+    fun returnANoClearCollection() {
+        runTrx({ this.guardarUnItem() }, listOf(TransactionType.MONGO), DataBaseType.TEST)
+        val result = runTrx({ this.obtenerTodos() }, listOf(TransactionType.MONGO), DataBaseType.TEST)
+        Assert.assertEquals(1, result.count())
+    }
+*/
+    @Test
+    fun testGettingItems() {
+        var result = runTrx({ this.dao.getAll() }, listOf(TransactionType.MONGO), DataBaseType.TEST)
+        Assert.assertEquals(this.items.toHashSet(), result.toHashSet())
+        runTrx({ this.borrarNItems(1) }, listOf(TransactionType.MONGO), DataBaseType.TEST)
+        result = runTrx({ this.dao.getAll() }, listOf(TransactionType.MONGO), DataBaseType.TEST)
+        Assert.assertEquals(this.items.toHashSet(), result.toHashSet())
+    }
+
+    abstract fun borrarNItems(n: Int)
+
+    @After
+    open fun deleteAll(){
+        runTrx({ this.dao.deleteAll() }, listOf(TransactionType.MONGO), DataBaseType.TEST)
+    }
+}
