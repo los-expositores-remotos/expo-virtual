a954ffc72826f22b84db7e3943d00b9d3a3897a8
adriancardozo <adrian.cardozo1@alu.unq.edu.ar>
2020-11-02 18:51:04 -0300

    renombro

--- src/main/kotlin/ar/edu/unq/API/Mappers.kt
+++ src/main/kotlin/ar/edu/unq/API/Mappers.kt
@@ -5,7 +5,7 @@ import kotlin.reflect.KVisibility
 import kotlin.reflect.full.memberProperties
 import kotlin.reflect.jvm.internal.impl.load.kotlin.JvmType
 
-inline fun <reified E:Any, T:Any> viewMapperTo(objeto: T): E{
+inline fun <reified E:Any, T:Any> mapTo(objeto: T): E{
     val objetoNuevo = E::class.java.newInstance()
     val propiedades = objeto::class.java.kotlin.memberProperties.filterIsInstance<KMutableProperty<*>>()
     val propiedadesNuevas = objetoNuevo::class.java.kotlin.memberProperties.filterIsInstance<KMutableProperty<*>>()
