38a44cbba97c89b4d7210253592b6a95b7cda910
barreirogustavo05 <barreirogustavo05@gmail.com>
2020-11-03 22:35:29 -0300

    imprementando pagina de empresas

--- frontend/src/components/routes/Suppliers.js
+++ frontend/src/components/routes/Suppliers.js
@@ -1,9 +1,59 @@
 import React from "react";
+import {useEffect, useState} from "react";
+
 
 const Suppliers = () => {
-  return (
+  const [companies, setCompanies] = useState(null)
+  useEffect(() => {
+    fetch(`http://localhost:7000/companies`, {
+      headers: {
+        "Content-Type":"application/json"
+      }
+    }) 
+      .then((res)=> {
+      console.log(res)
+      if(res.ok){
+        return res.json()
+      }
+    })
+    .then((result)=>{
+      console.log(result)
 
-   <p> estoy en el Suppliers </p>
+        setCompanies(result)        
+ 
+      console.log(companies)
+    })
+    .catch((err => {
+      console.log(err)
+    }))
+  }, [companies])
+
+
+  const getSProveedores = () =>{
+    if(companies){
+      const listOfCompanies = companies.map((company)=> {
+         
+        return  (<li>
+                  <p>{company.companyName}</p>
+              </li>)
+  
+      })
+      return (
+        <ul>
+          {listOfCompanies}
+        </ul>
+      )
+    }
+  }
+  return (
+    <div>
+          {
+            !companies ?
+              <p>Loading...</p>
+            :
+            getSProveedores()
+          }
+        </div>
 
   );
 };
