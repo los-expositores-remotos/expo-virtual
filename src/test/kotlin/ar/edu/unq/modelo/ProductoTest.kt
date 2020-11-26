c6d542adae68ab182b6dddc4f429c41f868fcaa1
Cristian Gonzalez <cristian.gonzalez.unq@gmail.com>
2020-11-26 18:02:54 -0300

    seIntegraMensajesDePago

--- src/test/kotlin/ar/edu/unq/modelo/ProductoTest.kt
+++ src/test/kotlin/ar/edu/unq/modelo/ProductoTest.kt
@@ -46,7 +46,7 @@ class ProductoTest : ModelObjectWithBsonIdTest<Producto>(Producto::class.java) {
         var pesoTotal = 0;
         val productoB = Producto(ObjectId(), "ElProducto", "SoyElProducto", 50, 22, 1000, 10, 10, 10, 200)
         val productoC = Producto(ObjectId(), "ElProducto", "SoyElProducto", 50, 22, 1000, 5, 30, 10, 10)
-        listOf(producto, productoB, productoC).map { volumenResultante += it.longitud!! * it.ancho!! * it.alto!!; pesoTotal += it.pesoKg!! }
+        listOf(producto, productoB, productoC).map { volumenResultante += it.longitud * it.ancho * it.alto; pesoTotal += it.pesoKg }
         assertEquals(3500, volumenResultante)
         assertEquals(310, pesoTotal)
     }
